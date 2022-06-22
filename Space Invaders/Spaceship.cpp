316d0ec62df01363eadd1b41be271440282174e8
Klim Krivoguzov <100575059+Phaseant@users.noreply.github.com>
2022-06-22 08:04:29 +0300

    Shorten if-conditions

--- Space Invaders/Spaceship.cpp
+++ Space Invaders/Spaceship.cpp
@@ -22,31 +22,19 @@ void Spaceship::SpaceshipControl(int height, int width)
 {
     const Uint8 * keystate = SDL_GetKeyboardState(NULL);
     if(keystate[SDL_SCANCODE_LEFT])
-    {
         xpos -= 5;
-    }
     if(keystate[SDL_SCANCODE_RIGHT])
-    {
         xpos += 5;
-    }
     
     if(xpos < 0)
-    {
         xpos = 3;
-    }
     else if (xpos > width - 76)
-    {
         xpos = width - 79;
-    }
     
     if(xpos < 0)
-    {
         xpos = 0;
-    }
     else if (ypos > height - 68)
-    {
         xpos = height - 68;
-    }
 }
 
 
